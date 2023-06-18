49d4cc9a886203049fd5ec409b09a976920e4b83
Kayque Goncalves <kayque.gsilva229@gmail.com>
2023-06-18 13:42:38 -0300

    feat: create index file to export home page components

---
+++ src/pages/home/components/index.ts
@@ -0,0 +1,3 @@
+export { default as NavigationMenu } from './navigation-menu/navigation-menu'
+export { default as ActionMenu } from './action-menu'
+export { default as MainContent } from './main-content'
