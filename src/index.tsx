e7fc7f8c3194e720fa09937b5e1a4353a1361e56
Kayque Goncalves <kayque.gsilva229@gmail.com>
2023-06-18 13:37:47 -0300

    feat: create index.tsx pages router structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {Home, Login, Signup} from '../../../juno-application/my-app/src/pages'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import './global.css'
+import {PlayerContextProvider} from "./contexts/player-context";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <PlayerContextProvider>
+      <RouterProvider router={router} />
+    </PlayerContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
