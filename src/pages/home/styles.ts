b07e744312e82700c6188309eb3f3800deda301e
Kayque Goncalves <kayque.gsilva229@gmail.com>
2023-06-18 13:53:33 -0300

    feat: create styles for home page

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  
+  justify-content: space-between;
+  
+  background: #313131;
+  
+  height: 100vh;
+`
