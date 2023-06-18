c256773aecb1a86c1925e819f6d874a3312c7b94
Kayque Goncalves <kayque.gsilva229@gmail.com>
2023-06-18 13:50:11 -0300

    feat: create menu-items constant

---
+++ src/constants/menu-items.tsx
@@ -0,0 +1,35 @@
+export const mainMenuItems = [
+  {
+    icon: 'home',
+    title: 'Home',
+    isActive: true,
+  },
+  {
+    icon: 'explore',
+    title: 'Descubra',
+    isActive: false,
+  },
+  {
+    icon: 'music',
+    title: 'Trending',
+    isActive: false,
+  },
+]
+
+export const collectionItems = [
+  {
+    icon: 'inbox',
+    title: 'Playlists',
+    isActive: false,
+  },
+  {
+    icon: 'user',
+    title: 'Artistas',
+    isActive: false,
+  },
+  {
+    icon: 'heart',
+    title: 'Meus favoritos',
+    isActive: false,
+  },
+]
