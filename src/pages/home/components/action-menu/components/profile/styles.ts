3c9fb47dac2468354b0b476e64f7ab1f35257780
Kayque Goncalves <kayque.gsilva229@gmail.com>
2023-06-18 13:52:09 -0300

    feat: create styles for profile component

---
+++ src/pages/home/components/action-menu/components/profile/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ProfilePhoto = styled.div`
+  width: 40px;
+  height: 40px;
+
+  background: #474747;
+  border-radius: 100px;
+  
+  > img {
+    width: 40px;
+    height: 40px;
+    
+    border-radius: 100px;
+  }
+`
