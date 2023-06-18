95e30f1abb4d1494076e45e923a3546789569bc6
Kayque Goncalves <kayque.gsilva229@gmail.com>
2023-06-18 13:53:43 -0300

    feat: create styles for login page

---
+++ src/pages/login/styles.ts
@@ -0,0 +1,202 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  
+  background: #313131;
+`;
+
+export const LeftSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  max-width: 40vw;
+  width: 100%;
+  
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 5.07%, rgba(36, 36, 36, 0.455106) 63.02%, #242424 99.6%);
+`;
+
+export const RightSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  max-width: 60vw;
+  width: 100%;
+`
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  
+  align-items: center;
+  justify-content: center;
+  
+  gap: 16px;
+  
+  > h1 {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 64px;
+    line-height: 77px;
+    color: #D9D9D9;
+  }
+`
+
+export const Logo = styled.div`
+  width: 39px;
+  height: 59px;
+
+  background: #D9D9D9;
+  border-radius: 8px;
+`
+
+export const RightSectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  max-width: 420px;
+  width: 100%;
+`
+
+export const GreetingsContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  
+  margin-bottom: 60px;
+  
+  gap: 14px;
+  
+  > h2 {
+    font-family: Inter sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 29px;
+
+    color: #FFFFFF;
+  }
+  
+  > h3 {
+    font-family: Inter sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+
+    color: #929292;
+  }
+`
+
+export const FormContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  gap: 30px;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  
+  align-items: center;
+  justify-content: center;
+  
+  padding: 5px;
+  gap: 10px;
+  
+  border: 1px solid #929292;
+  border-radius: 8px;
+  
+  &:hover {
+    border: 1px solid #5C5C5C;
+  }
+`
+
+export const TextInput = styled.input`
+  flex: 1 0;
+  
+  min-width: 50px;
+  min-height: 25px;
+  
+  background-color: transparent;
+  padding-left: 5px;
+  border: 0;
+  
+  color: #929292;
+  font-size: 16px;
+  
+  &:focus {
+    outline: none;
+  }
+`
+
+export const Icon = styled.div`
+  flex: 0 0;
+  height: 24px;
+`;
+
+export const ForgotPasswordContainer = styled.div`
+  display: flex;
+
+  justify-content: flex-end;
+  margin-top: 10px;
+  
+  > span {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 16px;
+
+    color: #486ADE;
+    font-family: Inter sans-serif;
+  }
+`
+
+export const ActionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  
+  align-items: center;
+  justify-content: space-between;
+  
+  margin-top: 60px;
+  
+  > button {
+    width: 141px;
+    height: 40px;
+    
+    border: none;
+
+    background: #486ADE;
+    border-radius: 10px;
+    
+    cursor: pointer;
+    
+    color: #FFFFFF;
+    font-family: Inter sans-serif;
+  }
+  
+  > span {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    font-family: Inter sans-serif;
+
+    color: #929292;
+    
+    > a {
+      color: #486ADE;
+      
+      cursor: pointer;
+      text-decoration: none;
+      font-weight: 400;
+      font-family: Inter sans-serif;
+    }
+  }
+`
+
